572ca3b6ee4d31e4d9d0b19c20865001347ee98f
mnbf9fca <robert.aleck@cynexia.com>
2019-10-06 21:58:50 +0100

    working

--- src/simple_robot_arm.ino
+++ src/simple_robot_arm.ino
@@ -3,50 +3,34 @@
 // simple_robot_arm                                   //
 // designed to work with a simple 4DOF robot arm      //
 // with 4 servo motors                                //
-// which receives a JSON instructing angle of any one //
-// or more servos                                     //
+// which receives a joint angle published via ROS for //
+// one or more servos as <servo>/<angle> on /servo    //
 // MIT licence                                        //
 // from https://github.com/mnbf9rca/simple_robot_arm  //
 //                                                    //
 /******************************************************/
 
-#include <ArduinoJson.h>
+// ROS includes
+#define ROSSERIAL_ARDUINO_TCP
+#include <ros.h>
+#include <std_msgs/String.h> // we publish servo angles as a string #/###
+#define ROS_SERVO_TOPIC servo
 
-// JSON key names for the 4 servos
-#define SERVO_BASE_NAME "base"
-#define SERVO_LEFT_NAME "left"
-#define SERVO_RIGHT_NAME "right"
-#define SERVO_GRIP_NAME "grip"
+// ROS parameters
+// Server settings
+IPAddress server(192, 168, 17, 29); // IP of ROS server
+uint16_t serverPort = 11411;
 
-// pinout for each servo
-#define SERVO_BASE_PIN D0
-#define SERVO_LEFT_PIN D1
-#define SERVO_RIGHT_PIN D2
-#define SERVO_GRIP_PIN D3
+// ROS timing
+uint16_t period = 1000;
+uint32_t last_time = 0;
 
-//min and max ranges for each servo
-#define SERVO_BASE_MIN 0
-#define SERVO_BASE_MAX 180
-#define SERVO_LEFT_MIN 30
-#define SERVO_LEFT_MAX 80
-#define SERVO_RIGHT_MIN 60
-#define SERVO_RIGHT_MAX 150
-#define SERVO_GRIP_MIN 5
-#define SERVO_GRIP_MAX 40
+// Servo parameters
+Servo servo[4];                                                  // holds the servo objects
+int servoMinMax[][2] = {{0, 180}, {30, 80}, {60, 150}, {5, 40}}; //set of {min, max} values for each servo
+int servoPins[] = {D0, D1, D2, D3};                              // which pin us each servo attached to
 
-// macros to accomodate different size key names
-#define JSON_TEMPLATE "{\"%s\":%d,\"%s\":%d,\"%s\":%d,\"%s\":%d}"
-#define LENGTH_OF_JSON_KEYS (sizeof(SERVO_BASE_NAME) + sizeof(SERVO_LEFT_NAME) + sizeof(SERVO_RIGHT_NAME) + sizeof(SERVO_GRIP_NAME))
-
-Servo servoBase;  // base
-Servo servoLeft;  // left
-Servo servoRight; // right
-Servo servoGrip;  // gripper
-
-int targetBase = 90;
-int targetLeft = 30;
-int targetRight = 90;
-int targetGrip = 20;
+int targetAngle[] = {90, 30, 90, 20};
 
 // helper function to publish integers e.g. for debugging
 void publishInt(const char *name, int integer)
@@ -73,80 +57,131 @@ int midpoint(int minimum, int maximum)
   return minimum + ((maximum - minimum) / 2);
 }
 
+// ROS
+ros::NodeHandle nh;
+void servo_cb(const std_msgs::String &cmd_msg)
+{
+  // expecting it to be int/int e.g. 1/120 to set servo 1 to 120
+  const int instructionAlloc = 6; // #/## +1 for \n
+  char instruction[instructionAlloc];
+
+  int cx;
+  // safely capture the first instructionAlloc characters of the string
+  //return is the number of characters printed (excluding the null byte used to end output to strings)
+  // only when this returned value is non-negative and less than n, the string has been completely written
+  // http://www.cplusplus.com/reference/cstdio/snprintf/
+  cx = snprintf(instruction, instructionAlloc, cmd_msg.data);
+  // check that the instruction fitted in to the buffer
+  if ((cx < 0) || (cx > instructionAlloc - 1)) //less 1 for \n
+  {
+    publishInt("Message contains too many characters", cx);
+    return;
+  }
+  
+  // minimum instruction size is 3: 0/0
+  if (cx < 3)
+  {
+    publishInt("Message contains too few characters", cx);
+    return;
+  }
+
+  // pull 1st char and cast to int
+  if (!isdigit(instruction[0]))
+  {
+    publishChar("first character not digit", &instruction[0]);
+    return;
+  }
+  int servoToSet = instruction[0] - '0';
+  // publishInt("found servo", servoToSet);
+
+  // check that 2nd char is /
+  if (!(instruction[1] == '/'))
+  {
+    publishChar("Second character not /", &instruction[1]);
+    return;
+  }
+  // check that chars at 2 to cx are digits
+  for (int i = 2; i < cx; i++)
+  {
+    if (!isdigit(instruction[i]))
+    {
+      publishChar("character not digit", &instruction[i]);
+      return;
+    }
+  }
+  char buf[instructionAlloc-2]; //hold the extracted characters in a buffer, maximum size is instructionAlloc-2
+  char *p = instruction + 2; // create pointer to start 2 chars in
+  strncpy(buf, p, cx - 2);
+
+  buf[sizeof(buf) - 1] = '\0'; //remember to null terminate string
+
+  targetAngle[servoToSet] = atoi(buf);
+
+  // publishInt("setting to", targetAngle[servoToSet]);
+  setServos();
+}
+ros::Subscriber<std_msgs::String> sub("servo", servo_cb);
+
+// servo control
+void setServos()
+{
+  for (size_t i = 0; i < 4; i++)
+  {
+    servo[i].write(boundValue(&targetAngle[i],      // target
+                              &servoMinMax[i][0],   //min
+                              &servoMinMax[i][1])); //max
+  }
+  // toggle LED to indicate complete
+  digitalWrite(D7, HIGH - digitalRead(D7));
+}
+
 void toggleServos()
 {
   // when starting up, servos seem to get stuck. this moves them to one way then another.
-
-  char *buf;                                              //hold the JSON string
-  buf = (char *)malloc(sizeof(JSON_TEMPLATE) -            //raw template
-                       16 * sizeof(char) +                // 4 x %s and 4 x %d
-                       LENGTH_OF_JSON_KEYS +              // the length of the text of the JSON keys
-                       4 * snprintf(NULL, 0, "%d", 180)); // 4 integer values
-  if (buf == NULL)
+  for (size_t i = 0; i < 4; i++)
   {
-    publishChar("Out of memory", "toggleServos");
-    free(buf);
-    return;
+    targetAngle[i] = servoMinMax[i][0];
   }
-  // first, let's set to the min values
-  sprintf(buf,
-          JSON_TEMPLATE,
-          SERVO_BASE_NAME,
-          SERVO_BASE_MIN,
-          SERVO_LEFT_NAME,
-          SERVO_LEFT_MIN,
-          SERVO_RIGHT_NAME,
-          SERVO_RIGHT_MIN,
-          SERVO_GRIP_NAME,
-          SERVO_GRIP_MIN);
 
-  parseAndStoreJson(buf);
   setServos();
   delay(1000); // wait for it to move
+  for (size_t i = 0; i < 4; i++)
+  {
+    targetAngle[i] = midpoint(servoMinMax[i][0], servoMinMax[i][1]);
+  }
 
   // now return all to midpoints
-  sprintf(buf,
-          JSON_TEMPLATE,
-          SERVO_BASE_NAME,
-          midpoint(SERVO_BASE_MIN, SERVO_BASE_MAX),
-          SERVO_LEFT_NAME,
-          midpoint(SERVO_LEFT_MIN, SERVO_LEFT_MAX),
-          SERVO_RIGHT_NAME,
-          midpoint(SERVO_RIGHT_MIN, SERVO_RIGHT_MAX),
-          SERVO_GRIP_NAME,
-          midpoint(SERVO_GRIP_MIN, SERVO_GRIP_MAX));
 
-  parseAndStoreJson(buf);
   setServos();
-  free(buf);
 }
 
 void setup()
 {
-  if (!didAttachServer(servoBase.attach(SERVO_BASE_PIN), 0))
+  for (size_t i = 0; i < 4; i++)
   {
-    return;
-  } //base
-  if (!didAttachServer(servoLeft.attach(SERVO_LEFT_PIN), 1))
-  {
-    return;
-  } //left
-  if (!didAttachServer(servoRight.attach(SERVO_RIGHT_PIN), 2))
-  {
-    return;
-  } //right
-  if (!didAttachServer(servoGrip.attach(SERVO_GRIP_PIN), 3))
-  {
-    return;
-  } //gripper
+    if (!didAttachServer(servo[i].attach(servoPins[i]), i))
+    {
+      return;
+    }
+  }
 
-  Particle.function("moveServoFunc", moveServoFunc);
+  // Set the connection to rosserial socket server
+  nh.getHardware()->setConnection(server, serverPort);
+  nh.initNode();
+  char IP[] = "xxx.xxx.xxx.xxx";
+  IPAddress ip = (nh.getHardware()->getLocalIP());
+  ip.toString().toCharArray(IP, 16);
+
+  publishChar("publishing from IP", IP);
+
+  // subscribe to topic
+  nh.subscribe(sub);
 
   // to let us monitor the values
-  Particle.variable("targetBase", targetBase);
-  Particle.variable("targetLeft", targetLeft);
-  Particle.variable("targetRight", targetRight);
-  Particle.variable("targetGrip", targetGrip);
+  Particle.variable("targetAngle[0]", targetAngle[0]);
+  Particle.variable("targetAngle[1]", targetAngle[1]);
+  Particle.variable("targetAngle[2]", targetAngle[2]);
+  Particle.variable("targetAngle[3]", targetAngle[3]);
   pinMode(D7, OUTPUT);
   toggleServos();
 }
@@ -166,178 +201,33 @@ bool didAttachServer(bool result, uint16_t pin)
   return result;
 }
 
-int moveServoFunc(String extra)
+int boundValue(int *receivedValue, int *minValue, int *maxValue)
 {
-  parseAndStoreJson(extra);
-  setServos();
-  return 0;
-}
-
-/***************
-/
-/ parse + store JSON
-/
-***************/
-bool parseJson(JsonDocument(*doc), const char *json)
-// Parse the json document, return in *doc, bool if ok
-{
-  DeserializationError err = deserializeJson(*doc, json);
-  if (err)
+  if (*receivedValue > *maxValue)
   {
-    publishChar("DeserializationError", err.c_str());
-    return false;
+    return *maxValue;
   }
-  else
+  else if (*receivedValue < *minValue)
   {
-    publishChar("deserialized json", json);
-    return true;
+    return *minValue;
   }
-}
-
-bool safeSetTargetValue(int *target, int *receivedValue, int minValue, int maxValue)
-// compares the receivedValue to min + max, and if it's between them, assigns it to *target
-{
-  if ((*receivedValue >= minValue) && (*receivedValue <= maxValue))
-  {
-    *target = *receivedValue;
-    return true;
-  }
-  return false;
-}
-
-void checkIfOutsideRange(int *target, int *receivedValue, const char *servoName)
-// checks if a value is wihtin allowed range for this specific servo
-{
-  int servo_min = 0;
-  int servo_max = 0;
-
-  if (strcmp(servoName, SERVO_BASE_NAME) == 0)
-  {
-    servo_min = SERVO_BASE_MIN;
-    servo_max = SERVO_BASE_MAX;
-  }
-  else if (strcmp(servoName, SERVO_LEFT_NAME) == 0)
-  {
-    servo_min = SERVO_LEFT_MIN;
-    servo_max = SERVO_LEFT_MAX;
-  }
-  else if (strcmp(servoName, SERVO_RIGHT_NAME) == 0)
-  {
-    servo_min = SERVO_RIGHT_MIN;
-    servo_max = SERVO_RIGHT_MAX;
-  }
-  else if (strcmp(servoName, SERVO_GRIP_NAME) == 0)
-  {
-    servo_min = SERVO_GRIP_MIN;
-    servo_max = SERVO_GRIP_MAX;
-  }
-  if (!safeSetTargetValue(target, receivedValue, servo_min, servo_max))
-  {
-    char *buf;
-    // "{s: %s, v: %d, min: %d, max: %d}";
-    size_t buffersz = (32 - 8) * sizeof(char) +                 //template less %s + 3 x %d
-                      strlen(servoName) +                       //name of the servo
-                      snprintf(NULL, 0, "%d", *receivedValue) + // requested value
-                      snprintf(NULL, 0, "%d", servo_min) +      // min value
-                      snprintf(NULL, 0, "%d", servo_max) +      // max value
-                      1;
-    buf = (char *)malloc(buffersz);
-
-    if (buf == NULL)
-    {
-      publishChar("Out of memory", "safeSetTargetValue");
-      free(buf);
-      return;
-    }
-    snprintf(buf, buffersz, "{s: %s, v: %d, min: %d, max: %d}", servoName, *receivedValue, servo_min, servo_max);
-
-    publishChar("failed to set servo", buf);
-    free(buf);
-  }
-}
-
-// fetches a value if it exists in teh JSON
-bool safeGetKeyValue(JsonObject(*obj), int *target, const char *(valueToFetch))
-{
-  if (obj->containsKey(valueToFetch))
-  {
-    *target = obj->getMember(valueToFetch).as<int>();
-    return true;
-  }
-  return false;
-}
-
-// fetches the value if it exists, then checks if it's inside the valid range before setting
-void fetchValueAndSet(JsonObject(*objptr), int(*targetPtr), const char *(valueToFetch))
-{
-  int *receivedValuePtr = (int *)malloc(sizeof(int));
-  if (receivedValuePtr == NULL)
-  {
-    publishChar("Out of memory", "fetchValueAndSet");
-    free(receivedValuePtr);
-    return;
-  }
-  if (safeGetKeyValue(objptr, receivedValuePtr, valueToFetch))
-  {
-    checkIfOutsideRange(targetPtr, receivedValuePtr, valueToFetch);
-  }
-  free(receivedValuePtr);
-}
-
-void parseAndStoreJson(const char *json)
-{
-  // receive the JSON for the servo states and store
-
-  const size_t capacity = JSON_OBJECT_SIZE(4) + LENGTH_OF_JSON_KEYS;
-  StaticJsonDocument<capacity> doc;
-
-  if (!parseJson(&doc, json))
-  {
-    // didnt get a document...
-    return;
-  }
-
-  // fetch values and set servos
-  JsonObject obj = doc.as<JsonObject>();
-
-  fetchValueAndSet(&obj, &targetBase, SERVO_BASE_NAME);
-  fetchValueAndSet(&obj, &targetLeft, SERVO_LEFT_NAME);
-  fetchValueAndSet(&obj, &targetRight, SERVO_RIGHT_NAME);
-  fetchValueAndSet(&obj, &targetGrip, SERVO_GRIP_NAME);
-
-  doc.clear(); // release memory
-}
-
-/***************
-/
-/ servo control
-/
-***************/
-void setServos()
-{
-  // sets all 4 servos to the currently set values
-  publishChar("setting servos", "");
-  publishInt("targetBase", targetBase);
-  writeToServo(&servoBase, targetBase);
-  publishInt("targetLeft", targetLeft);
-  writeToServo(&servoLeft, targetLeft);
-  publishInt("targetRight", targetRight);
-  writeToServo(&servoRight, targetRight);
-  publishInt("targetGrip", targetGrip);
-  writeToServo(&servoGrip, targetGrip);
-
-  // toggle LED to indicate complete
-  digitalWrite(D7, HIGH - digitalRead(D7));
-  publishChar("exiting", "setServos");
-}
-void writeToServo(Servo *servo, int target)
-{
-  //  writes a specific servo
-  // separated out to allow e.g. a delay to be added etc.
-  servo->write(target);
-  delay(2);
+  return *receivedValue;
 }
 
 void loop()
 {
+  if (millis() - last_time >= period)
+  {
+    last_time = millis();
+    if (nh.connected())
+    {
+      publishChar("Connected", "");
+    }
+    else
+    {
+      publishChar("Not Connected", "");
+    }
+  }
+  nh.spinOnce();
+  delay(1);
 }
